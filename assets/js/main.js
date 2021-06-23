1306a49ca6bf69caa623ac06644a8af2ceeed062
Francisca-Heii <franciscahheii@gmail.com>
2021-06-23 13:09:06 +0000

    Added wildlife images to Chobe National Park

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,34 @@
 $(document).ready(function () {
+    //Places to explore hover
     $('.article-figure-1')
         .hover(() => {
             $('.article-image-hover-1').css('display', 'block');
         })
         .mouseleave(event => {
-            const target = event.target;
             $('.article-image-hover-1').css('display', 'none');
         });
+
     $('.article-figure-2')
         .hover(() => {
             $('.article-image-hover-2').css('display', 'block');
         })
         .mouseleave(event => {
-            const target = event.target;
             $('.article-image-hover-2').css('display', 'none');
+        });
+
+    //Wild life hover functionality
+    $('.wildlife img')
+        .mouseenter(event => {
+            let imageName = event.target.getAttribute('data-image');
+            if (imageName) {
+                const imageUrl = 'assets/images/animals/hover/' + imageName;
+                $('.animal-tooltip img').attr('src', imageUrl);
+                $('.animal-tooltip').css('display', 'block');
+                $('.animal-tooltip').css('top', event.pageY);
+                $('.animal-tooltip').css('left', event.pageX);
+            }
+        })
+        .mouseleave(() => {
+            $('.animal-tooltip').css('display', 'none');
         });
 });
