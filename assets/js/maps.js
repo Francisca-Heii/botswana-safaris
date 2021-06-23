1700655f79489c81c6a307122498903b2a56169d
Francisca-Heii <franciscahheii@gmail.com>
2021-06-23 13:56:50 +0000

    Okavango banner image added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -10,21 +10,7 @@ function initMap() {
     const labels = ['Okavango Delta', 'Chobe National Park'];
 
     const locations = [Okavango, Chobe];
-    // Add some markers to the map.
-    // Note: The code uses the JavaScript Array.prototype.map() method to
-    // create an array of markers based on a given "locations" array.
-    // The map() method here has nothing to do with the Google Maps API.
-    // const markers = locations.map((location, i) => {
-    //     return new google.maps.Marker({
-    //         position: location,
-    //         label: labels[i % labels.length],
-    //     });
-    // });
-    // Add a marker clusterer to manage the markers.
-    // new MarkerClusterer(map, markers, {
-    //     imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-    // });
-
+    
     const OkavangoHtml = `
        <img src="assets/images/okavango-delta_450_300.png" width=250 height=200>
        <h5 style="text-align:center"> Okavango Delta </h5>
@@ -72,5 +58,4 @@ function initMap() {
     });
 }
 
-// { lat: -19.318701, lng: 22.446918 }, okavango
-// { lat: -17.8291147, lng: 25.144448 } chobe
+
