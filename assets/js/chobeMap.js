488c7dd068fb60f1f7c44ffdcedbeefdb63bbad1
Francisca-Heii <franciscahheii@gmail.com>
2021-06-23 19:03:50 +0000

    Added Okavango map for accomodation, game resrve and restuarants

--- assets/js/chobeMap.js
+++ assets/js/chobeMap.js
@@ -1,42 +1,42 @@
 //This method is called from google map API after load
-function initOkavangoMap() {
+function initChobeMap() {
     //Create a map object using map element
-    const map = new google.maps.Map(document.getElementById('okavango-map'), {
-        zoom: 8,
-        center: { lat: -19.653257, lng: 22.906792 },
+    const map = new google.maps.Map(document.getElementById('chobe-map'), {
+        zoom: 6,
+        center: { lat: -19.653257, lng: 25.906792 },
     });
 
     //Creating list of places array for all the map markers
     const places = [
         {
-            name: 'Motsentsela Tree Lodge',
+            name: 'Cresta Mowana Hotel',
             type: 'Accomodation',
-            coords: { lat: -20.0112396, lng: 23.4153301 },
+            coords: { lat: -18.2286385, lng: 24.3632235 },
         },
         {
-            name: 'Maun Lodge',
+            name: 'Garden Lodge',
             type: 'Accomodation',
-            coords: { lat: -20.0048179, lng: 23.426011 },
+            coords: { lat: -17.8022269, lng: 25.1553267 },
         },
         {
-            name: 'Moremi Game Reserve',
+            name: 'Chobe Game Lodge',
             type: 'GameReserve',
-            coords: { lat: -19.435872, lng: 22.490276 },
+            coords: { lat: -17.84083, lng: 22.6738693 },
         },
         {
-            name: 'Camp Xakanaxa',
+            name: 'Linyanti Wildlife Reserve',
             type: 'GameReserve',
-            coords: { lat: -19.1849423, lng: 22.9373825 },
+            coords: { lat: -18.2278243, lng: 25.3632207 },
         },
         {
-            name: 'Hilarys Restaurant',
+            name: 'The Coffee Buzz',
             type: 'Restaurant',
-            coords: { lat: -19.9777666, lng: 23.4248807 },
+            coords: { lat: -19.2274172, lng: 24.4632192 },
         },
         {
-            name: 'Miguels Taco Shack Restaurant',
+            name: 'Nandos restaurant Kasane',
             type: 'Restaurant',
-            coords: { lat: -19.9757143, lng: 23.4314571 },
+            coords: { lat: -20.2270102, lng: 24.5642178 },
         },
     ];
 
@@ -50,14 +50,12 @@ function initOkavangoMap() {
         const infowindow = new google.maps.InfoWindow({
             content: infohtml,
         });
-
         //Create the map marker which will be shown on google map
         const marker = new google.maps.Marker({
             position: place.coords,
             map,
             title: place.name,
         });
-
         //Attaching the click event on marker to show the info window when clicked
         marker.addListener('click', () => {
             infowindow.open({
